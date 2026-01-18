60c254af09631e020af078b7de997813b871d45c
maruthiram08 <ram.venkat578@gmail.com>
2026-01-18 14:17:05 +0530

    Fix: Use percentage strings for ResizablePanel sizes (v4 treats numbers as pixels)

--- app/src/components/editor/ThreePaneLayout.tsx
+++ app/src/components/editor/ThreePaneLayout.tsx
@@ -15,7 +15,7 @@ export function ThreePaneLayout({ leftPane, middlePane, rightPane }: ThreePaneLa
             <ResizablePanelGroup orientation="horizontal">
 
                 {/* Left Pane: Sidebar List (15%) */}
-                <ResizablePanel defaultSize={15} minSize={10} maxSize={25} className="bg-gray-50/30">
+                <ResizablePanel defaultSize="15" minSize="10" maxSize="25" className="bg-gray-50/30">
                     <div className="h-full border-r border-gray-100 flex flex-col">
                         {leftPane}
                     </div>
@@ -24,7 +24,7 @@ export function ThreePaneLayout({ leftPane, middlePane, rightPane }: ThreePaneLa
                 <ResizableHandle withHandle={false} className="w-px bg-gray-100 hover:bg-gray-300 transition-colors" />
 
                 {/* Middle Pane: Main Editor (55%) */}
-                <ResizablePanel defaultSize={55} minSize={30} className="bg-white">
+                <ResizablePanel defaultSize="55" minSize="30" className="bg-white">
                     <div className="h-full flex flex-col min-w-0">
                         {middlePane}
                     </div>
@@ -33,7 +33,7 @@ export function ThreePaneLayout({ leftPane, middlePane, rightPane }: ThreePaneLa
                 <ResizableHandle withHandle={false} className="w-px bg-gray-100 hover:bg-gray-300 transition-colors" />
 
                 {/* Right Pane: Agent Companion (30%) */}
-                <ResizablePanel defaultSize={30} minSize={20} maxSize={45} className="bg-white">
+                <ResizablePanel defaultSize="30" minSize="20" maxSize="45" className="bg-white">
                     <div className="h-full border-l border-gray-100 flex flex-col">
                         {rightPane}
                     </div>
